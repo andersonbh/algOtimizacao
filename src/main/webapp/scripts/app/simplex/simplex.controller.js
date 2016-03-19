08401f9924fa53f91337ee405eb25423756e9234
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2016-03-19 18:24:20 -0300

    Mudanca da pagina simplex

--- src/main/webapp/scripts/app/simplex/simplex.controller.js
+++ src/main/webapp/scripts/app/simplex/simplex.controller.js
@@ -2,6 +2,23 @@
 
 angular.module('algotimizacaoApp')
     .controller('SimplexController', function ($scope, Principal) {
+        $scope.indice = 0;
+        $scope.variaveis = [];
+        $scope.restricoes = [];
+
+        $scope.init = function(){
+            $scope.variaveis.push(0);
+            $scope.restricoes.push(1)
+        };
+
+        $scope.addVar = function(){
+            $scope.variaveis.push($scope.variaveis.length);
+        };
+
+        $scope.addRestricao = function(){
+          $scope.restricoes.push($scope.restricoes.length + 1);
+        };
+
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
