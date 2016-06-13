1c2b54512ede786e889fcce6d45e3be4a26ae873
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2016-06-13 18:55:40 -0300

    Mudanca do nome do arquivo
    devido a conflito com nome da classe, o arquivo foi renomeado.

--- src/main/java/br/pucminas/algotimizacao/config/model/simplexModel.java
+++ src/main/java/br/pucminas/algotimizacao/config/model/simplexModel.java
@@ -10,9 +10,11 @@ public class SimplexModel {
     private int numeroDeRestricoes; // numero de restricoes
     private int tamanhoFuncaoObjetivo; // tamanho da funcao objetivo
 
-    private boolean maximizarMinimizar; // boolean, caso queira maximizar sera true, senao false
+    private boolean maximizarMinimizar; // caso queira maximizar sera true, senao false
 
     private int[] variaveisBasicas; // variaveisBasicas[i] = variaveis basicas da posicao i
+
+    private boolean IMPOSSIVEL = false;
 
     public SimplexModel(double[][] tabela, int numeroDeRestricoes, int tamanhoFuncaoObjetivo, boolean maximizarMinimizar) {
         this.maximizarMinimizar = maximizarMinimizar;
@@ -27,9 +29,7 @@ public class SimplexModel {
         resolverSimplex();
     }
 
-
     /**
-     *
      * @param funcaoObjetivo é a funcao objetivo
      * @param restricoesLadoEsquerdo é o lado esquerdo da equacao, antes do limite
      * @param restricoesLadoDireito é o lado direito da equacao, depois do limite
@@ -37,69 +37,60 @@ public class SimplexModel {
      * @param maxMin pode ser true para Maximizar ou false para Minimizar
      */
     public static String[] iniciar(double[] funcaoObjetivo, double[][] restricoesLadoEsquerdo, double [] restricoesLadoDireito, int[] limites, boolean maxMin) {
-
-        /*
-        Funcao exemplo testada:
-        MaxZ = 40 X1 + 30 X2
-        3 X1 + 4 X2 <= 12
-        7 X1 + 2 X 2 <= 14
-        X1, X2 >= 0
-
-        Resultado:
-        MaxZ= 115,45
-        X1= 1,91
-        X2= 1,45
-        X3= 0
-        X4= 0
-        */
-
         Model model = new Model(restricoesLadoEsquerdo, restricoesLadoDireito, limites, funcaoObjetivo);
 
         SimplexModel simplex = new SimplexModel(model.getTabela(), model.getNumeroDeRestricoes(), model.getTamanhoFuncaoObjetivo(), maxMin);
         double[] x = simplex.primal();
+        String[] resultado;
+        if(simplex.IMPOSSIVEL){
+            resultado = new String[1];
+            resultado[0] = "Solução impossível";
+        }else{
+           resultado = new String[x.length + 1];
+            for (int i = 0; i < x.length; i++) {
+                resultado [i]= "x[" + (i + 1) + "] = " + x[i]  ;
+                System.out.println("x[" + (i + 1) + "] = " + x[i]);
+            }
 
-        String[] resultado = new String[x.length + 1];
-        for (int i = 0; i < x.length; i++) {
-            resultado [i]= "x[" + (i + 1) + "] = " + x[i]  ;
-            System.out.println("x[" + (i + 1) + "] = " + x[i]);
+            resultado [x.length] = "Solucao: " + simplex.getSolucao();
+            System.out.println("Solucao: " + simplex.getSolucao());
         }
 
-        resultado [x.length]= "Solução: " + simplex.value();
-        System.out.println("Solucao: " + simplex.value());
 
         return resultado;
     }
 
     private void resolverSimplex() {
         while (true) {
-
             mostrar();
             int q = 0;
-            // find entering column q
+
+            // Encontra coluna q
             if (maximizarMinimizar) {
-                q = dantzig();
+                q = getMaiorPositivo();
             } else {
-                q = dantzigNegative();
+                q = getMenorNegativo();
             }
             if (q == -1)
                 break; // optimal
 
-            // find leaving row p
-            int p = minRatioRule(q);
-            if (p == -1)
-                throw new ArithmeticException("Linear program is unbounded");
+            // Encontra linha p
+            int p = regraTaxaMinima(q);
+            if (p == -1) {
+                IMPOSSIVEL = true;
+                break;
+            }
 
-            // pivot
+            // Pivo
             pivot(p, q);
 
-            // update variaveisBasicas
+            // Atualiza as variaveis basicas
             variaveisBasicas[p] = q;
-
         }
     }
 
-    // index of a non-basic column with most positive cost
-    private int dantzig() {
+    // Index da coluna nao-basica com maior custo positivo
+    private int getMaiorPositivo() {
         int q = 0;
         for (int j = 1; j < numeroDeRestricoes + tamanhoFuncaoObjetivo; j++)
             if (tabela[numeroDeRestricoes][j] > tabela[numeroDeRestricoes][q])
@@ -111,93 +102,85 @@ public class SimplexModel {
             return q;
     }
 
-    // index of a non-basic column with most negative cost
-    private int dantzigNegative() {
+    // Index da coluna nao basica com custo mais negativo
+    private int getMenorNegativo() {
         int q = 0;
         for (int j = 1; j < numeroDeRestricoes + tamanhoFuncaoObjetivo; j++)
             if (tabela[numeroDeRestricoes][j] < tabela[numeroDeRestricoes][q])
                 q = j;
 
         if (tabela[numeroDeRestricoes][q] >= 0)
-            return -1; // optimal
+            return -1; // Otimo
         else
             return q;
     }
 
-    // find row p using min ratio rule (-1 if no such row)
-    private int minRatioRule(int q) {
+    // Encontra linha p usando a regra da taxa minima (-1 se nao existe)
+    private int regraTaxaMinima(int q) {
         int p = -1;
         for (int i = 0; i < numeroDeRestricoes; i++) {
             if (tabela[i][q] <= 0)
                 continue;
             else if (p == -1)
                 p = i;
-            else if ((tabela[i][numeroDeRestricoes
-                + tamanhoFuncaoObjetivo] / tabela[i][q]) < (tabela[p][numeroDeRestricoes
-                + tamanhoFuncaoObjetivo] / tabela[p][q]))
+            else if ((tabela[i][numeroDeRestricoes + tamanhoFuncaoObjetivo] / tabela[i][q]) < (tabela[p][numeroDeRestricoes + tamanhoFuncaoObjetivo] / tabela[p][q]))
                 p = i;
         }
         return p;
     }
 
-    // pivot on entry (p, q) using Gauss-Jordan elimination
+    // Pivota a entrada (p, q) usando eliminacao
     private void pivot(int p, int q) {
-
-        // everything but row p and column q
+        // Tudo alem da linha p e coluna q
         for (int i = 0; i <= numeroDeRestricoes; i++)
             for (int j = 0; j <= numeroDeRestricoes
                 + tamanhoFuncaoObjetivo; j++)
                 if (i != p && j != q)
-                    tabela[i][j] -= tabela[p][j] * tabela[i][q]
-                        / tabela[p][q];
+                    tabela[i][j] -= tabela[p][j] * tabela[i][q] / tabela[p][q];
 
-        // zero out column q
+        // Coloca zero na coluna q
         for (int i = 0; i <= numeroDeRestricoes; i++)
             if (i != p)
                 tabela[i][q] = 0.0;
 
-        // scale row p
+        // Escala linha p
         for (int j = 0; j <= numeroDeRestricoes + tamanhoFuncaoObjetivo; j++)
             if (j != q)
                 tabela[p][j] /= tabela[p][q];
+
         tabela[p][q] = 1.0;
     }
 
-    // return optimal objective value
-    public double value() {
-        return -tabela[numeroDeRestricoes][numeroDeRestricoes
-            + tamanhoFuncaoObjetivo];
+    // Retorna solucao
+    public double getSolucao() {
+        return -tabela[numeroDeRestricoes][numeroDeRestricoes + tamanhoFuncaoObjetivo];
     }
 
-    // return primal solution vector
+    // Retorna vetor de solucao primal
     public double[] primal() {
         double[] x = new double[tamanhoFuncaoObjetivo];
         for (int i = 0; i < numeroDeRestricoes; i++)
             if (variaveisBasicas[i] < tamanhoFuncaoObjetivo)
-                x[variaveisBasicas[i]] = tabela[i][numeroDeRestricoes
-                    + tamanhoFuncaoObjetivo];
+                x[variaveisBasicas[i]] = tabela[i][numeroDeRestricoes + tamanhoFuncaoObjetivo];
         return x;
     }
 
-    // print tabela
+    // Exibe tabela
     public void mostrar() {
         System.out.println("M = " + numeroDeRestricoes);
         System.out.println("N = " + tamanhoFuncaoObjetivo);
         for (int i = 0; i <= numeroDeRestricoes; i++) {
-            for (int j = 0; j <= numeroDeRestricoes
-                + tamanhoFuncaoObjetivo; j++) {
+            for (int j = 0; j <= numeroDeRestricoes + tamanhoFuncaoObjetivo; j++) {
                 System.out.printf("%7.2f ", tabela[i][j]);
             }
             System.out.println();
         }
-        System.out.println("value = " + value());
+
+        System.out.println("Solucao = " + getSolucao());
         for (int i = 0; i < numeroDeRestricoes; i++)
             if (variaveisBasicas[i] < tamanhoFuncaoObjetivo)
-                System.out.println("x_"
-                    + variaveisBasicas[i]
-                    + " = "
-                    + tabela[i][numeroDeRestricoes
-                    + tamanhoFuncaoObjetivo]);
+                System.out.println("x_" + variaveisBasicas[i] + " = " + tabela[i][numeroDeRestricoes + tamanhoFuncaoObjetivo]);
+
         System.out.println();
     }
 }
